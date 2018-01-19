f7b6767be320049a6e7ba1d091453fe60cf83499
Prasoon Birla <birla.prasoon1@gmail.com>
2018-01-19 18:57:50 +0530

    COmpleted the app!

--- app/src/main/java/com/example/hppc/earthquake/MainActivity.java
+++ app/src/main/java/com/example/hppc/earthquake/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.hppc.earthquake;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,26 +35,60 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Get a reference to connectivity manager to check the Network Connectivity.
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //Get the active network info.
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        //If there is a network connection, fetch data
+        if(networkInfo!=null && networkInfo.isConnected()){
+
+            //Get a reference to loader manager in order to Interact with the loaders
+            LoaderManager loaderManager = getLoaderManager();
+
+            //Initialize the loader, pass the Int ID, and pass null for bundle.
+            //Pass in this activity for the loaderCallbacks parameter.
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID,null,this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.progressBar);
+            loadingIndicator.setVisibility(View.GONE);
+            // Update empty state with no connection error message
+            updateUI(null,"No Internet Connection!");
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_LONG;
+            Toast noInternetConnectionToast = Toast.makeText(context,"No Internet Connection!",duration);
+            //noInternetConnectionToast.setText("No Internet Connection!");
+
+            noInternetConnectionToast.show();
+        }
 
         /*LoaderManager loaderManager = getLoaderManager();
         loaderManager.initLoader(EARTHQUAKE_LOADER_ID,null,this);*/
 
-        //Async helper to run the network programming in the background
+        /*//Async helper to run the network programming in the background
         AsyncHelper helper = new AsyncHelper();
-        helper.execute(stringURL);
+        helper.execute(stringURL);*/
     }
 
-    private void updateUI(ArrayList<EarthQuake> earthQuakes) {
+    private void updateUI(ArrayList<EarthQuake> earthQuakes,String defaultStatus) {
         ListView listView = findViewById(R.id.lv);
         if(earthQuakes==null||earthQuakes.isEmpty()){
             TextView emptyTextView = findViewById(R.id.emptyView);
-            emptyTextView.setText(R.string.no_Data_Found);
+            emptyTextView.setText(defaultStatus);
             listView.setEmptyView(emptyTextView);
+            updateStatus(listView,"No data found");
             return;
         }
         UserAdapter userAdapter = new UserAdapter(this,earthQuakes);
         listView.setAdapter(userAdapter);
         handleAdapterTouchEvent(listView,userAdapter);
+    }
+
+    private void updateStatus(ListView listView,String msg) {
+
     }
 
 
@@ -83,8 +121,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<EarthQuake>> onCreateLoader(int id, Bundle args) {
-        EarthQuakeLoader earthQuakeLoader = new EarthQuakeLoader(this,stringURL);
-        return earthQuakeLoader;
+        return new EarthQuakeLoader(this,stringURL);
     }
 
     @Override
@@ -93,7 +130,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         ProgressBar progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.GONE);
         if(earthQuakes!=null && !earthQuakes.isEmpty()){
-            updateUI((ArrayList)earthQuakes);
+            updateUI((ArrayList)earthQuakes,"No earthquake found!");
         }
 
         //updateUI(null);
@@ -103,12 +140,12 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(Loader<List<EarthQuake>> loader) {
         //adapter's data isn't valid now
-        updateUI(new ArrayList<EarthQuake>());
+        updateUI(null,"No earthQUakeFound");
 
     }
 
 
-    private class AsyncHelper extends AsyncTask<String,Void,ArrayList<EarthQuake>>{
+    /*private class AsyncHelper extends AsyncTask<String,Void,ArrayList<EarthQuake>>{
 
         @Override
         protected ArrayList<EarthQuake> doInBackground(String... strings) {
@@ -123,5 +160,5 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             progressBar.setVisibility(View.GONE);
             updateUI(earthQuakes);
         }
-    }
+    }*/
 }
